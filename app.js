cc3962c786fe209d3817d1c03eebe01d40a188d0
Shaiakhmetov Amir <shaiakhmetovamir2006@gmail.com>
2025-02-18 11:51:36 +0600

    add pieces section

--- app.js
+++ app.js
@@ -26,3 +26,20 @@ function updateCarousel() {
     indicators.forEach(ind => ind.classList.remove("active"));
     indicators[index].classList.add("active");
 }
+
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
